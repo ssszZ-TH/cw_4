f6bf85a5fb8998c0e79ace7496abc43c5f653bd8
ssszZ window11_tufA15 <spachitralada@gmail.com>
2023-07-20 01:52:12 +0700

    นอนละ เบื่อ

--- demo.py
+++ demo.py
@@ -1,44 +1,29 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-# Load the image
-image = cv2.imread("input.jpg")
+# read the input image
+img = cv2.imread('input.jpg',cv2.IMREAD_GRAYSCALE)
 
-# Convert the image to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# find the discrete fourier transform of the image
+dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
 
-# Convert the image to floating point
-gray_image = np.float32(gray_image)
+# hift zero-frequency component to the center of the spectrum
+dft_shift = np.fft.fftshift(dft)
+rows, cols = img.shape
+crow,ccol = rows//2 , cols//2
+mask = np.zeros((rows,cols,2),np.uint8)
+mask[crow-30:crow+30, ccol-30:ccol+30] = 1
 
-# Get the Fourier transform of the image
-fft = cv2.dft(gray_image, flags=cv2.DFT_COMPLEX_OUTPUT)
+# apply mask and inverse DFT
+fshift = dft_shift*mask
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
 
-# Shift the zero frequency component to the center of the spectrum
-fft_shifted = np.fft.fftshift(fft)
-
-# Calculate the magnitude of the Fourier transform
-magnitude = np.abs(fft_shifted)
-
-# Convert the magnitude to an image
-magnitude_image = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
-
-# Inverse Fourier transform the magnitude image
-ifft = cv2.idft(magnitude_image)
-
-# Shift the zero frequency component back to the corner of the spectrum
-ifft_shifted = np.fft.ifftshift(ifft)
-
-# Get the real part of the inverse Fourier transform
-ifft_image = np.real(ifft_shifted)
-
-# Display the original image, the Fourier transform, the magnitude image, and the inverse Fourier transform
-cv2.imshow("Original", image)
-cv2.waitKey(0)
-
-cv2.imshow("Inverse Fourier", ifft_image)
-cv2.waitKey(0)
-
-# Wait for the user to press a key
-
-# Close all windows
-cv2.destroyAllWindows()
+# visualize the images
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
